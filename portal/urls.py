79fccb0a6bb7a31d5b58649df662e44e58b78691
David Jorge Bustamante <david.j.bustamante@gmail.com>
2023-09-23 20:29:45 -0300

    23/09/2023 primera entrega parcial

--- portal/urls.py
+++ portal/urls.py
@@ -4,5 +4,11 @@ from . import views as portal_views
 
 urlpatterns = [
     path('',portal_views.indice,name="indice"),
-    path('cursos/<int:porte>', portal_views.cursos,name="cursos"),
+    path('busqueda/', portal_views.busqueda,name="busqueda"),
+    path('colaboracion/', portal_views.colaboracion,name="colaboracion"),
+    path('nosotros/',portal_views.nosotros,name="nosotros"),
+    path('administracion/',portal_views.administracion, name="administracion"),
+    path('sesion/',portal_views.sesion,name="sesion"),
+    path('registrar/',portal_views.registrar,name="registrar"),
+    # path('novedades/',portal_views.novedades,name="novedades"),
 ]
--- portal/views.py
+++ portal/views.py
@@ -1,10 +1,58 @@
 from django.shortcuts import render, HttpResponse
 from datetime import datetime 
 
+import datetime
+
+# Define una función para calcular la diferencia entre la fecha actual y la fecha de cada registro
+def diferencia_fecha(registro):
+    # Obtén la fecha actual
+    fecha_actual = datetime.date.today()
+    return abs(registro["fecha"] - fecha_actual)
+
 def indice(request):
-    respuesta = render(request,"portal/index.html",{"ahora": datetime.now()})
+    # Supongamos que tienes una lista de diccionarios con registros y fechas:
+    registros = [
+        {"url_icono": "portal/img/icon_11.png", "usuario": "Roberto Noooni", "nombre": "Nombre Proyecto 1", "detalle": "asdasdad asdasdasd asdadsa", "fecha": datetime.date(2023, 9, 15)},
+        {"url_icono": "portal/img/icon_12.png", "usuario": "Carlos Yutooni", "nombre": "Nombre Proyecto 2", "detalle": "asdasdad asdasdasd asdadsa", "fecha": datetime.date(2023, 9, 20)},
+        {"url_icono": "portal/img/icon_13.png", "usuario": "Juano PPooni", "nombre": "Nombre Proyecto 3", "detalle": "asdasdad asdasdasd asdadsa", "fecha": datetime.date(2023, 9, 1)},
+        {"url_icono": "portal/img/icon_14.png", "usuario": "Pablo Frffooni", "nombre": "Nombre Proyecto 4", "detalle": "asdasdad asdasdasd asdadsa", "fecha": datetime.date(2023, 9, 21)},
+        {"url_icono": "portal/img/icon_15.png", "usuario": "Daniel ZZooni", "nombre": "Nombre Proyecto 5", "detalle": "asdasdad asdasdasd asdadsa", "fecha": datetime.date(2023, 9, 5)},
+        {"url_icono": "portal/img/icon_16.png", "usuario": "Miguel Lleoni", "nombre": "Nombre Proyecto 6", "detalle": "asdasdad asdasdasd asdadsa", "fecha": datetime.date(2023, 9, 19)},
+    ]
+   
+    # Ordena los registros por proximidad a la fecha actual
+    registros_ordenados = sorted(registros, key=diferencia_fecha)
+    
+    # Selecciona los primeros 4 registros
+    registros_seleccionados = registros_ordenados[:4]
+    
+    respuesta = render(request,"portal/index.html",{"novedades": registros_seleccionados})
     return respuesta
 
-def cursos(request, porte):
-    respuesta = HttpResponse(f"Todos los cursos {porte}")
+def busqueda(request):
+    respuesta = render(request,"portal/busqueda.html")
+    return respuesta
+
+def nosotros(request):
+    respuesta = render(request,"portal/nosotros.html")
+    return respuesta
+
+def colaboracion(request):
+    respuesta = render(request,"portal/colaboracion.html")
+    return respuesta
+
+def administracion(request):
+    respuesta = render(request,"portal/administracion.html")
+    return respuesta
+
+def busqueda(request):
+    respuesta = render(request,"portal/busqueda.html")
+    return respuesta
+
+def registrar(request):
+    respuesta = render(request,"portal/registrar.html")
+    return respuesta
+
+def sesion(request):
+    respuesta = render(request,"portal/sesion.html")
     return respuesta
