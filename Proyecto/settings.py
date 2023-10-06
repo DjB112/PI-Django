153e4270b07b80a3fc7e8e9f00c534f160f08ef9
David Jorge Bustamante <david.j.bustamante@gmail.com>
2023-10-06 17:25:21 -0300

    06/10/2023 17:24 Conectar a base de datos

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t#my0u_pke=b0dfy4^m#c$0-$uatqh5z)#g)sukwcgxox#^(a6'
-
+# SECRET_KEY = 'django-insecure-t#my0u_pke=b0dfy4^m#c$0-$uatqh5z)#g)sukwcgxox#^(a6'
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'proyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST':config('DATABASE_HOST'),
+        'PORT':config('DATABASE_PORT'),
+        'NAME':config('DATABASE_NAME'),
+        'USER':config('DATABASE_USER'),
+        'PASSWORD':config('DATABASE_PASSWORD')
     }
 }
 
