b9b31c6925fa716abb174e253c38020a5fbe1c23
David Jorge Bustamante <david.j.bustamante@gmail.com>
2023-10-08 23:06:28 -0300

    08/10/2023 Validacion de formulario de contacto

--- portal/forms.py
+++ portal/forms.py
@@ -1,17 +1,48 @@
 from django import forms
+from django.forms import ValidationError
+import re
+
+# Realizo una validacion sobre los campos que solo pueden ser letras
+def solo_caracteres(value):
+    if any(char.isdigit() for char in value):
+        raise ValidationError('El nombre no puede contener números. %(valor)s',
+                              code='Invalid',
+                              params={'valor': value})
+
+#  Realizo una validacion dobre el campo mail para que no se utilice simbolos incorrectos
+def custom_validate_email(value):
+    email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if not re.match(email_regex, value):
+        raise ValidationError('Correo electrónico inválido')
 
 class ConsultaForm (forms.Form):
+    sector= (
+        ('','Seleccionar'),
+        (1,'Administracion'),
+        (2,'Colaboracion'),
+        (3,'Proyectos')
+    )
+    
     nombre = forms.CharField(
         label="Nombre:",
         max_length=20, 
-        required=True, 
+        required=True,
+        validators=(solo_caracteres,), 
         widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Ingresa tu nombre'})
+        )
+    apellido = forms.CharField(
+        label="Apellido:", 
+        max_length=20,
+        required=True,
+        validators=(solo_caracteres,),
+        widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Ingresa tu Apellido'})
         )
     email = forms.EmailField(
         label="Email:",
         max_length=50,
         required=True,
-        widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Ingresa tu email'})
+        validators=(custom_validate_email,),
+        widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Ingresa tu email','type':'email'})
         )
     edad = forms.IntegerField(
         label="Edad:", 
@@ -20,10 +51,15 @@ class ConsultaForm (forms.Form):
         max_value=90, 
         widget=forms.NumberInput(attrs={'class':'form-control w-25','placeholder':'Ingresa tu edad'})
         )
+    departamento = forms.ChoiceField(
+        label="Departamento:",
+        choices= sector,
+        required=True,
+        widget=forms.Select(attrs={'class':'form-control','placeholder':''})
+    )
     consulta = forms.CharField(
         label="Consulta:", 
         required=True,
-        min_length=12,
         max_length=200, 
         widget=forms.Textarea(attrs={'rows':10, 'class':'form-control','placeholder':'Ingresa tu consulta máximo 200 caracteres.'})
         )
@@ -37,3 +73,23 @@ class ConsultaForm (forms.Form):
         required=True,
         widget=forms.CheckboxInput(attrs={'class': 'form-check-input', 'value': 1})
     )
+    
+    # Si el mensaje tiene menos de 10 caracteres no resulta claro
+    def clean_consulta(self):
+        data = self.cleaned_data['consulta']
+        if len(data) < 10:
+            raise ValidationError(
+                "Debes especificar mejor el mensaje que nos envias")
+        return data
+    
+    #  Si la persona tiene menos de 18 años no puede enviar mensaje a los sectores que se requiere mayor de edad
+    def clean(self):
+        cleaned_data = super().clean()
+        nombre = cleaned_data.get("nombre")
+        edad = cleaned_data.get("edad")
+        departamento= cleaned_data.get("departamento")
+
+        if ((edad < 18) and (departamento=="2" or departamento=="3")):
+            msg = f"{nombre}, para colaborar o presentar proyectos debe tener como minimo 18 Años"
+            self.add_error('edad',msg)
+            raise ValidationError(msg)
